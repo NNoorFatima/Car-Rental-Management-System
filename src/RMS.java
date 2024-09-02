97249f068d68550f3dc4fc39c078c9daba2af3c8
Noor <noor.2205.ng@gmail.com>
2024-09-02 11:59:53 +0500

    completed renter management system class

--- src/RMS.java
+++ src/RMS.java
@@ -13,5 +13,64 @@ public class RMS {
 		renters= new ArrayList<>();	
 		renters.add(r);
 	}
+	public void addRenters(Renter r)
+	{
+		renters.add(r);
+	}
+	public void displayRenter()
+	{
+		for(Renter a: renters)	//displays renters 
+		{
+			a.displayRenterDetails();
+		}
+	}
+	public void removeRenter(int ID) //removing renters using specific id 
+	{
+		int index=0;
+		for (Renter a: renters)
+		{
+			//remove renter if no cars are rented 
+			if(a.getRentedCars()==null)
+			{
+				renters.remove(index);
+			}
+			else if( a.getRentedCars()!=null)
+			{
+				System.out.println("Sorry this renter has rented cars so can't remove them");
+			}
+			else
+			{
+				System.out.println("Sorry id is not available");
+			}
+			index++;
+		}
+		
+	}
+	public void removeRenter()
+	{
+		
+
+		int index=0;
+		for (Renter a: renters)
+		{
+			//remove renter if no cars are rented 
+			if(a.getRentedCars()==null)
+			{
+				renters.remove(index);
+			}
+			else if( a.getRentedCars()!=null)
+			{
+				System.out.println("Sorry this renter has rented cars so can't remove them");
+			}
+			else
+			{
+				System.out.println("Sorry id is not available");
+			}
+			index++;
+		}
+	}
+	
+	
+	
 
 }
